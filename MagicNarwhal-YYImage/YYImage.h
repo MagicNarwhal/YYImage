af343641f0415274fd8d50c403179cb7e9d68dcb
Eugene B <ebessilko@mera.ru>
2017-09-01 16:03:31 +0300

    update import statements;

--- MagicNarwhal-YYImage/YYImage.h
+++ MagicNarwhal-YYImage/YYImage.h
@@ -11,13 +11,13 @@
 
 #import <UIKit/UIKit.h>
 
-#if __has_include(<YYImage/YYImage.h>)
+#if __has_include(<MagicNarwhal-YYImage/YYImage.h>)
 FOUNDATION_EXPORT double YYImageVersionNumber;
 FOUNDATION_EXPORT const unsigned char YYImageVersionString[];
-#import <YYImage/YYFrameImage.h>
-#import <YYImage/YYSpriteSheetImage.h>
-#import <YYImage/YYImageCoder.h>
-#import <YYImage/YYAnimatedImageView.h>
+#import <MagicNarwhal-YYImage/YYFrameImage.h>
+#import <MagicNarwhal-YYImage/YYSpriteSheetImage.h>
+#import <MagicNarwhal-YYImage/YYImageCoder.h>
+#import <MagicNarwhal-YYImage/YYAnimatedImageView.h>
 #elif __has_include(<YYWebImage/YYImage.h>)
 #import <YYWebImage/YYFrameImage.h>
 #import <YYWebImage/YYSpriteSheetImage.h>
